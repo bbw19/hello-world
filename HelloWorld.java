cd92c59a669ae6ecd17476ba6970804999a54aa6
bbw19 <julien.readler@lernende.bbw.ch>
2020-10-02 08:11:15 +0200

    first on new branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
+    
+    private String hello = "Hello World!";
+
     public HelloWorld() {
-        
+
+    }
+
+    public String getHello() {
+        return hello;
     }
 }
